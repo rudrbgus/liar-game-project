52371813153a4356366f34d4780daa12e50f5722
rudrbgus <rudrbgus3@gmail.com>
2023-10-07 17:51:57 +0900

    [라이어 게임] - 헤더 작업중

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,9 +1,10 @@
-import './App.css';
-// import './style.scss';
+import './App.scss';
+import Header from '../components/header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <p>안녕하세요</p>
     </div>
   );
