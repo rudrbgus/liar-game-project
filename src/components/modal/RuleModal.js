c79551b1f31b67a7d07a6f36fc049c0ab92d4ecc
rudrbgus <rudrbgus3@gmail.com>
2023-10-11 21:35:42 +0900

    [modal] - 규칙 모달 대충 완성 1.0ver

--- src/components/modal/RuleModal.js
+++ src/components/modal/RuleModal.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './RuleModal.scss';
 
 
 const RuleModal = ({click}) => {
+    const [Animation, setAnimation] = useState("__body mount");
     const clickModalFrameHandler = (event) =>{
         console.log(event.target.className);
         if(event.target.className === 'modal-frame'){
-            click();
+            setAnimation("__body unmount");
+            setTimeout(click(), 2000);            
         }
         
-    }
-    console.log("응애 나 애기 모달");
+    }    
     return (
         <div className='modal-frame' onClick={clickModalFrameHandler}> 
-            <div className='__body'>
+            <div className={Animation}>
                 <div className='__content'>                    
                     <span className='title'>게임 설명</span>
-                    <span><span className='blue'>참가자</span>들은 1명의 <span className='red'>라이어</span>와 나머지 참가자로 이루어집니다</span>    
-                    <span>안뇽</span>
+                    <hr></hr>
+                    <p>참가자 들은 똑같은 제시어를 받습니다 <br/>하지만 1명의 라이어는 제시어를 받지 못합니다<br/> 참가자들은 각자의 제시어를 설명해서<br/> 자신이
+                    라이어가 아님을 밝히고 라이어는 <br/>자신이 라이어라는 사실을 들키지 않고 살아남는 게임입니다.                        
+                    </p>                    
+                    <span></span>
                 </div>
             </div>
         </div>
