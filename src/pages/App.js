ba276bcb7d13de8a6368b95644e743d5f83df20c
rudrbgus <rudrbgus3@gmail.com>
2023-10-13 16:54:34 +0900

    [game-page] - init

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,13 +1,22 @@
 import './App.scss';
 import Header from '../components/header/Header';
 import Main from './Main';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <Main/>
-    </div>
-  );
+  const [screenNumber, setScreenNumber] = useState(1);  
+  const screenNumberToggleHandler = (change) =>{
+    
+  }
+  if(screenNumber === 1)
+  {
+    return (
+      <div className="App">
+        <Main/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
