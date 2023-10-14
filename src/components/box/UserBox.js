89c6a01ec45436db5152369c1ae52d99dcfda9aa
rudrbgus <rudrbgus3@gmail.com>
2023-10-14 16:50:29 +0900

    [game-page]- 대충 만듬

---
+++ src/components/box/UserBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./UserBox.scss";
+
+const UserBox = () => {
+  return (
+    <div className='box'>
+        <span>1번 참가자</span>
+    </div>
+  )
+}
+
+export default UserBox
--- src/pages/InGamePage.js
+++ src/pages/InGamePage.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import "./InGamePage.scss";
+import UserBox from '../components/box/UserBox';
 
 const InGamePage = () => {
   return (
-    <div>InGamePage</div>
+    <div className='wrapper'>
+      <div className='user-box-left-part'>        
+        <UserBox/>
+        <UserBox/>
+        <UserBox/>
+        <UserBox/>
+      </div>
+      <div className='chat-part'>
+        채팅파트
+      </div>
+      <div className='user-box-right-part'>
+        <UserBox/>
+        <UserBox/>
+        <UserBox/>
+        <UserBox/>
+      </div>
+    </div>
   )
 }
 
-export default InGamePage;
+export default InGamePage;  
