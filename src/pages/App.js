9484695f4621728cda15f9f52845d4770df4c01f
rudrbgus <rudrbgus3@gmail.com>
2023-11-13 23:31:21 +0900

    [game-page] - 서버 연동중
    스프링 이용해서 서버 연동중 어려워 현재 같이 연동하는거 하는중임

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,18 @@
 import './App.scss';
-import Header from '../components/header/Header';
 import Main from './Main';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import InGamePage from './InGamePage';
+import axios from 'axios';
+
 
 function App() {
+  const [hello, setHello] = useState('')
+
+    useEffect(() => {
+        axios.get('/api/hello')
+        .then(response => setHello(response.data))
+        .catch(error => console.log(error))
+    }, []);
   const [screenNumber, setScreenNumber] = useState(1);  
   const screenNumberToggleHandler = (change) =>{
       setScreenNumber(change);
@@ -14,16 +22,21 @@ function App() {
   if(screenNumber === 1)
   {
     return (
+      <>
       <div className="App">
         <Main change ={screenNumberToggleHandler}/>
       </div>
+      <div>{hello}</div>
+      </>
     );
   }else if(screenNumber === 2){
     return(
+      <>
       <InGamePage/>
+      <div>{hello}</div>
+      </>
     )
+    }
   }
-  
-}
 
 export default App;
