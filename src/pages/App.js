2a88b86de17c97734b4d2b9eacba4edb2be7041c
rudrbgus <rudrbgus3@gmail.com>
2023-12-14 15:34:10 +0900

    웹 소켓 설정중
    필요한 라이브러리 설치 
    "npm install sockjs-client stompjs"

--- src/pages/App.js
+++ src/pages/App.js
@@ -9,6 +9,28 @@ function App() {
   const screenNumberToggleHandler = (change) =>{ // 화면 체인지 하는 핸들러
       setScreenNumber(change);
   }
+
+
+  const connect = () => {
+    var socket = new SockJS('/ws');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+      console.log('Connected: ' + frame);
+      stompClient.subscribe('/topic/messages', function (response) {
+      showMessage(JSON.parse(response.body).content);
+      });
+    });
+  }
+
+  const showMessage = (message) => {
+    // 메시지를 처리하는 로직
+    console.log('Received: ' + message);
+}
+
+  const sendMessage= () => {
+    const message = { content: $('#message').val() };
+    stompClient.send("/app/chat", {}, JSON.stringify(message));
+}
   
   // 스크린이 1이면 Main 아니면 InGame 화면
   if(screenNumber === 1)
@@ -17,6 +39,7 @@ function App() {
       <>
         <div className="App">
           <Main change ={screenNumberToggleHandler}  />
+          <button onclick="sendMessage()">Send</button>
         </div>        
       </>
     );
